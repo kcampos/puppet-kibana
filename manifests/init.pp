ec25b8a66cb5d657ae418d7fdc0b9103e37280df
Kyle Campos <kyle.campos@gmail.com>
2014-04-02 10:34:16 -0700

    Refactor and set elasticsearch server in config.js

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,19 @@
 class kibana(
+  $elasticsearch_server,
   $version      = $kibana::params::version,
   $download_dir = $kibana::params::download_dir,
   ) inherits kibana::params {
-	
-  file { $download_dir: ensure => directory }
 
-  archive{"kibana-${version}":
+  archive{ $install_dir:
     url      => $package_url,
-    target   => $download_dir,
-    checksum => false,
-    require  => File[$download_dir]
+    target   => $download_path,
+    checksum => false
+  }
+
+  exec { "set-elasticsearch-server":
+    command => "sed -i -r -e 's|elasticsearch:.+|elasticsearch: \"${elasticsearch_server}\"|' ${install_path}/config.js",
+    path    => "/usr/bin:/usr/sbin:/bin:/usr/local/bin",
+    unless  => "grep 'elasticsearch: \"${elasticsearch_server}\"' ${install_path}/config.js",
+    require => Archive[$install_dir]
   }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,5 +1,7 @@
 class kibana::params {
-  $version      = "3.0.0"
-  $package_url  = "https://download.elasticsearch.org/kibana/kibana/kibana-${version}.tar.gz"
-  $download_dir = "/usr/share/kibana3"
+  $version       = "3.0.0"
+  $package_url   = "https://download.elasticsearch.org/kibana/kibana/kibana-${version}.tar.gz"
+  $download_path = "/usr/share"
+  $install_dir   = "kibana-${version}"
+  $install_path  = "${download_dir}/${install_dir}"
 }
